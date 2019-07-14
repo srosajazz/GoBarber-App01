1fe684fa5b1a0fddec7401e948c052d7df3fe3b8
Sergio Rosa <scarlosjazz@gmail.com>
2019-07-14 11:05:02 -0400

    Migration and scheduling model

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -4,7 +4,7 @@ class FileController {
   async store(req, res) {
     const { originalname: name, filename: path } = req.file;
 
-    const file = await Files.create({
+    const file = await File.create({
       name,
       path,
     });
